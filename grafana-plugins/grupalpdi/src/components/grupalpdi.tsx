5e8d7483ae345d523039b2df79520063986ebf35
Mario <mandrexito87@gmail.com>
2021-10-31 15:38:27 -0500

    Se cambia a fondo negro plugin grupalPDI

--- grafana-plugins/grupalpdi/src/components/grupalpdi.tsx
+++ grafana-plugins/grupalpdi/src/components/grupalpdi.tsx
@@ -175,7 +175,7 @@ const Grupalpdi = ({DatosGenerales, ParametrosIn, ParametrosOut, Indicadores}: D
                     <path
                       id="rect818-4-5"
                       fillRule="evenodd"
-                      fill="url(#linearGradient701-6-2-2)"
+                      fill="black"//"url(#linearGradient701-6-2-2)"
                       paintOrder="markers stroke fill"
                       strokeWidth={1.9309}
                       d="M80.369 300.08H590.749V584.5H80.369z"
